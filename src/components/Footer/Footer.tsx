f5a9073c5e838bca778db38f2f074d66969ccf64
Ariel Behar <ariel.behar@hotmail.com>
2024-02-06 01:19:41 +0200

    Updating Footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,26 @@
+import styled from "@emotion/styled"
 import arielbeharLogo from "../../assets/img/logos/arielbehar-logo.png"
+
+const StyledFooter = styled('footer')`
+    position: absolute;
+    bottom: 0;
+
+    p {
+        a {
+            text-decoration: none;
+        }
+    }
+`
 
 function Footer() {
     return (
-        <footer className="w-100 d-flex flex-row justify-content-center ">
-            <p className="text-custom-dark-gray fw-bold">
+        <StyledFooter className="w-100 d-flex flex-row justify-content-center ">
+            <p className="text-custom-light-gray fw-bold">
                 &copy; 2020 All Rights Reserved&nbsp;
-                <a href="www.arielbehar.com" target="_blank" rel="noreferrer">Ariel Behar</a>
+                <a  href="www.arielbehar.com" target="_blank" rel="noreferrer">Ariel Behar</a>
                 <img className="ms-1" style={{ height: '20px' }} src={arielbeharLogo} alt="Ariel Behar Logo" />
             </p>
-        </footer>
+        </StyledFooter>
     )
 }
 
