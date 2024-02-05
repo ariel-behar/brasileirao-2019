6724ce522846004cef8ff0a93e2f7460b050110b
Ariel Behar <ariel.behar@hotmail.com>
2024-02-05 18:12:30 +0200

    Pushing the footer to the bottom of the page with styles on #root and main

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,14 +1,19 @@
-import { Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 
 function RootView() {
-  return (
-    <>
-        <Header />
-        <Outlet/>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<main className='flex-grow-1 bg-success'>
+				<Outlet />
+			</main>
+
+			<Footer />
+		</>
+	)
 }
 
 export default RootView
