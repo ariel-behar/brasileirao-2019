e2af3ae83ebe901cda37259fa35930d050f0e458
Ariel Behar <ariel.behar@hotmail.com>
2024-02-05 19:04:38 +0200

    Adding the teams images and modifying the teams.json

---
+++ src/components/Cards/NextMatchCard.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+
+const StyledNextMatchCard = styled('div')`
+    background-color: rgba(242, 250, 252, 0.8);
+    box-shadow: 0 0 40px 10px #f2fafc;
+
+`
+
+function NextMatchCard() {
+  return (
+    <StyledNextMatchCard>
+
+
+
+    </StyledNextMatchCard>
+  )
+}
+
+export default NextMatchCard
