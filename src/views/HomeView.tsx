78f2b52bb7bcd8ab8f6f040c442dac6fe6349cca
Ariel Behar <ariel.behar@hotmail.com>
2024-02-05 18:27:30 +0200

    Setting up the background for the HomeView

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,12 +1,19 @@
+import soccerField from '../assets/img/soccer-field.jpg'
+
 import HelmetSEO from '../components/HelmetSEO'
 
 function HomeView() {
 	return (
-		<>
+		<div style={{
+			backgroundImage: `url('${soccerField}')`,
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: 'cover',
+			height: '100%',
+		}}>
 			<HelmetSEO title="HOME" description="Home page of the fictional website about the Brasileirao 2019 Soccer Championship" />
 
 			<div>HomeView</div>
-		</>
+		</div>
 
 	)
 }
