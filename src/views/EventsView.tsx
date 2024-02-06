277597e302021d55393dd7639aece99e0ae2b2a2
Ariel Behar <ariel.behar@hotmail.com>
2024-02-06 16:46:38 +0200

    basic implementation of the EventsView

--- src/views/EventsView.tsx
+++ src/views/EventsView.tsx
@@ -1,13 +1,29 @@
+import eventsBackgroundImage from '../assets/img/events.jpg'
+
+import events from '../data/events.json'
+
+import hasPageHeaderFooter from "../HOC/hasPageHeaderFooter"
 import HelmetSEO from "../components/HelmetSEO"
+
+import Container from 'react-bootstrap/Container'
 
 function EventsView() {
     return (
         <>
             <HelmetSEO title="EVENTS" description="Events page of the fictional website about the Brasileirao 2019 Soccer Championship" />
 
-            <div>EventsView</div>
+            <Container>
+                <section className='my-5'>
+                    <h3>Browse Upcoming Brasileirão Events</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas officia cumque beatae, alias rerum a eum repellendus accusantium recusandae nobis aspernatur aut deleniti voluptatibus impedit, ipsa autem expedita illo. Odit.</p>
+                </section>
+            </Container>
         </>
     )
 }
 
-export default EventsView
+export default hasPageHeaderFooter(EventsView, {
+    viewTitle: 'Events',
+    pageHeaderImage: eventsBackgroundImage,
+    pageFooterImage: eventsBackgroundImage
+})
