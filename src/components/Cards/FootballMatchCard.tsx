3294c6beceb53276d6f59e385ff4cc13c4487cae
Ariel Behar <ariel.behar@hotmail.com>
2024-02-06 18:12:57 +0200

    Implementation of conditional styling and structure

--- src/components/Cards/FootballMatchCard.tsx
+++ src/components/Cards/FootballMatchCard.tsx
@@ -1,6 +1,7 @@
-import { Identifiable } from '../../types/shared-types';
-
 import { Link as RouterLink } from 'react-router-dom'
+
+import { Identifiable } from '../../types/shared-types';
+import getImageUrl from '../../utils/getImageUrl';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -8,7 +9,19 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
-import getImageUrl from '../../utils/getImageUrl';
+import Card from 'react-bootstrap/Card'
+
+const homeStyles = {
+    backgroundColor: 'rgba(242, 250, 252, 0.8)',
+    border: 'none',
+    boxShadow: '0 0 40px 10px #f2fafc',
+    borderRadius: '20px',
+    padding: '10px 20px'
+}
+
+const eventStyles = {
+    minHeight: '100%'
+}
 
 export interface Event extends Identifiable {
     teamOne: string;
@@ -25,57 +38,67 @@ export interface Event extends Identifiable {
 
 interface Props {
     event: Event
+    displayOnView: 'home' | 'events'
 }
 
 function FootballMatchCard({
-    event
+    event,
+    displayOnView
 }: Props) {
     return (
         <>
-            <Row className='p-3'>
-                <Col xs={3} className='d-flex justify-content-center'>
-                    <Image style={{maxHeight: '100px'}} className='team-logo' src={getImageUrl('teams', event.imageTeamOne)} alt={event.teamOne} fluid />
-                </Col>
-                <Col xs={6} className='d-flex flex-column justify-content-around align-items-center'>
-                    <h3 className='fw-bold'>Upcoming Match</h3>
-                    <p>
-                        <FontAwesomeIcon size='lg' icon="calendar" className='me-2' />
-                        {event.date}
-                    </p>
-                </Col>
-                <Col xs={3} className='d-flex justify-content-center'>
-                    <Image style={{maxHeight: '100px'}} className='team-logo' src={getImageUrl('teams', event.imageTeamTwo)} alt={event.teamTwo} fluid />
-                </Col>
-            </Row>
+            <Card className='shadow' style={displayOnView === 'home' ? homeStyles : eventStyles}>
+                <Card.Body className={`d-flex flex-column ${displayOnView === 'home' ? '' : 'py-2 px-0'}`}>
+                    <Row className={` g-0 flex-grow-1 ${displayOnView === 'home' ? 'p-3' : 'px-1'}`} >
+                        <Col xs={3} className='d-flex flex-column justify-content-between align-items-center'>
+                            <Image style={displayOnView === 'home' ? { maxHeight: '100px' } : { maxHeight: '50px' }} className='team-logo' src={getImageUrl('teams', event.imageTeamOne)} alt={event.teamOne} fluid />
+                            <p className='text-center'>{event.teamOne}</p>
+                        </Col>
+                        <Col xs={6} className='d-flex flex-column justify-content-around align-items-center'>
+                            <Card.Title className='fw-bold'>Upcoming Match</Card.Title>
+                            <p>
+                                <FontAwesomeIcon size='lg' icon="calendar" className='me-2' />
+                                {event.date}
+                            </p>
+                        </Col>
+                        <Col xs={3} className='d-flex flex-column justify-content-between align-items-center'>
+                            <Image style={displayOnView === 'home' ? { maxHeight: '100px' } : { maxHeight: '50px' }} className='team-logo' src={getImageUrl('teams', event.imageTeamTwo)} alt={event.teamTwo} fluid />
+                            <p className='text-center'>{event.teamTwo}</p>
+                        </Col>
+                    </Row>
 
-            <Row>
-                <Col xs={12} className='d-flex flex-column justify-content-center align-items-center '>
-                    <Image src={getImageUrl('stadiums', event.stadiumImage)} alt={event.stadium} fluid />
-                    <p className='mt-1'>Stadium: <span className='fw-bold'>{event.stadium}</span></p>
-                </Col>
-            </Row>
+                    <Row>
+                        <Col xs={12} className='d-flex flex-column justify-content-center align-items-center '>
+                            <Card.Img src={getImageUrl('stadiums', event.stadiumImage)} alt={event.stadium} />
+                            <p className='mt-1'>Stadium: <span className='fw-bold'>{event.stadium}</span></p>
+                        </Col>
+                    </Row>
 
-            <Row>
-                <Col xs={12} md={7} className='ps-4'>
-                    <p>
-                        <FontAwesomeIcon size='lg' icon="clock" className='me-2' />
-                        {event.hour}
-                    </p>
-                    <p>
-                        <FontAwesomeIcon size='lg' icon="location-dot" className='me-2' />
-                        {event.city}
-                    </p>
-                </Col>
+                    <Row>
+                        <Col xs={12} md={6} className='ps-4'>
+                            <p>
+                                <FontAwesomeIcon size='lg' icon="clock" className='me-2' />
+                                {event.hour}
+                            </p>
+                            <p>
+                                <FontAwesomeIcon size='lg' icon="location-dot" className='me-2' />
+                                {event.city}
+                            </p>
+                        </Col>
 
-                <Col xs={12} md={5} className='d-flex flex-row justify-content-center '>
-                    <RouterLink to="/tickets">
-                        <Button size='lg' variant='primary'>
-                            GET YOUR TICKET!
-                        </Button>
-                    </RouterLink>
+                        <Col xs={12} md={6} className='d-flex flex-row justify-content-center align-items-center'>
+                            <RouterLink to="/tickets">
+                                <Button
+                                    size={displayOnView === 'home' ? 'lg' : 'sm'}
+                                    variant='primary'>
+                                    GET YOUR TICKET!
+                                </Button>
+                            </RouterLink>
 
-                </Col>
-            </Row>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
         </>
     )
 }
