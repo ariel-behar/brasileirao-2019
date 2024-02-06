bebaa2d3ccf0b0705cdd82d27b37ef3bee2c6802
Ariel Behar <ariel.behar@hotmail.com>
2024-02-06 16:59:47 +0200

    Implementation of the FootballMatchCard into the HomeView

--- src/components/Cards/FootballMatchCard.tsx
+++ src/components/Cards/FootballMatchCard.tsx
@@ -1,9 +1,6 @@
+import { Identifiable } from '../../types/shared-types';
+
 import { Link as RouterLink } from 'react-router-dom'
-
-import styled from '@emotion/styled'
-
-import events from '../../../data/events.json'
-import getImageUrl from '../../../utils/getImageUrl'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -11,19 +8,9 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
+import getImageUrl from '../../utils/getImageUrl';
 
-const StyledNextMatchDisplay = styled('div')`
-    background-color: rgba(242, 250, 252, 0.8);
-    box-shadow: 0 0 40px 10px #f2fafc;
-    border-radius: 20px;
-    padding: 10px 20px;
-
-    .team-logo {
-        max-height: 100px;
-    } 
-`
-
-interface UpcomingMatch {
+export interface Event extends Identifiable {
     teamOne: string;
     imageTeamOne: string;
     teamTwo: string;
@@ -36,31 +23,35 @@ interface UpcomingMatch {
     price: number;
 }
 
-const upcomingMatch: UpcomingMatch = events[0];
+interface Props {
+    event: Event
+}
 
-function NextMatchDisplay() {
+function FootballMatchCard({
+    event
+}: Props) {
     return (
-        <StyledNextMatchDisplay>
+        <>
             <Row className='p-3'>
                 <Col xs={3} className='d-flex justify-content-center'>
-                    <Image className='team-logo' src={getImageUrl('teams', upcomingMatch.imageTeamOne)} alt={upcomingMatch.teamOne} fluid />
+                    <Image style={{maxHeight: '100px'}} className='team-logo' src={getImageUrl('teams', event.imageTeamOne)} alt={event.teamOne} fluid />
                 </Col>
                 <Col xs={6} className='d-flex flex-column justify-content-around align-items-center'>
                     <h3 className='fw-bold'>Upcoming Match</h3>
                     <p>
                         <FontAwesomeIcon size='lg' icon="calendar" className='me-2' />
-                        {upcomingMatch.date}
+                        {event.date}
                     </p>
                 </Col>
                 <Col xs={3} className='d-flex justify-content-center'>
-                    <Image className='team-logo' src={getImageUrl('teams', upcomingMatch.imageTeamTwo)} alt={upcomingMatch.teamTwo} fluid />
+                    <Image style={{maxHeight: '100px'}} className='team-logo' src={getImageUrl('teams', event.imageTeamTwo)} alt={event.teamTwo} fluid />
                 </Col>
             </Row>
 
             <Row>
                 <Col xs={12} className='d-flex flex-column justify-content-center align-items-center '>
-                    <Image src={getImageUrl('stadiums', upcomingMatch.stadiumImage)} alt={upcomingMatch.stadium} fluid />
-                    <p className='mt-1'>Stadium: <span className='fw-bold'>{upcomingMatch.stadium}</span></p>
+                    <Image src={getImageUrl('stadiums', event.stadiumImage)} alt={event.stadium} fluid />
+                    <p className='mt-1'>Stadium: <span className='fw-bold'>{event.stadium}</span></p>
                 </Col>
             </Row>
 
@@ -68,11 +59,11 @@ function NextMatchDisplay() {
                 <Col xs={12} md={7} className='ps-4'>
                     <p>
                         <FontAwesomeIcon size='lg' icon="clock" className='me-2' />
-                        {upcomingMatch.hour}
+                        {event.hour}
                     </p>
                     <p>
                         <FontAwesomeIcon size='lg' icon="location-dot" className='me-2' />
-                        {upcomingMatch.city}
+                        {event.city}
                     </p>
                 </Col>
 
@@ -85,10 +76,8 @@ function NextMatchDisplay() {
 
                 </Col>
             </Row>
-
-
-        </StyledNextMatchDisplay >
+        </>
     )
 }
 
-export default NextMatchDisplay
+export default FootballMatchCard
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,13 +1,26 @@
+import styled from '@emotion/styled'
+
 import soccerField from '../assets/img/backgrounds/soccer-field.jpg'
+import PlayerKicking from '../components/ViewSpecificComponents/HomeView/PlayerKicking'
+
+import events from '../data/events.json'
 
 import HelmetSEO from '../components/HelmetSEO'
-import PlayerKicking from '../components/ViewSpecificComponents/HomeView/PlayerKicking'
+import FootballMatchCard, { Event } from '../components/Cards/FootballMatchCard'
+import RankingDisplay from '../components/ViewSpecificComponents/HomeView/RankingDisplay';
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row"
 import Container from "react-bootstrap/Container";
-import NextMatchDisplay from '../components/ViewSpecificComponents/HomeView/NextMatchDisplay';
-import RankingDisplay from '../components/ViewSpecificComponents/HomeView/RankingDisplay';
+
+const upcomingMatch: Event = events[0];
+
+const StyledNextMatchDisplay = styled('div')`
+    background-color: rgba(242, 250, 252, 0.8);
+    box-shadow: 0 0 40px 10px #f2fafc;
+    border-radius: 20px;
+    padding: 10px 20px;
+`
 
 function HomeView() {
 	return (
@@ -24,8 +37,10 @@ function HomeView() {
 					<PlayerKicking />
 				</Col>
 
-				<Col xs={12} lg={{span: 4, offset: 1}}>
-					<NextMatchDisplay />
+				<Col xs={12} lg={{ span: 4, offset: 1 }}>
+					<StyledNextMatchDisplay>
+						<FootballMatchCard event={upcomingMatch} />
+					</StyledNextMatchDisplay >
 				</Col>
 
 				<Col xs={12} lg={3} className='mx-auto'>
