9c7b420391a535bd84a36a957a07da17b90fcb3b
Ariel Behar <ariel.behar@hotmail.com>
2024-02-07 08:16:01 +0200

    Implementation of the TicketsView and form into it

--- src/views/TicketsView.tsx
+++ src/views/TicketsView.tsx
@@ -1,14 +1,38 @@
+import ticket from '../assets/img/ticket.jpg'
+
+import hasPageHeaderFooter from "../HOC/hasPageHeaderFooter"
 import HelmetSEO from "../components/HelmetSEO"
+
+import Container from 'react-bootstrap/Container'
+import TicketsForm from '../components/ViewSpecificComponents/TicketsView/TicketsForm'
 
 function TicketsView() {
     return (
         <>
             <HelmetSEO title="TICKETS" description="Tickets page of the fictional website about the Brasileirao 2019 Soccer Championship" />
 
-            <div>TicketsView</div>
-        </>
+            <Container style={{minHeight: '50vh'}}>
+                <div className='my-5' >
+                    <h3>Order Your Tickets With Ease</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas officia cumque beatae, alias rerum a eum repellendus accusantium recusandae.</p>
+                </div>
 
+                <section style={{
+                    backgroundColor: '#e4efff',
+                    borderRadius: '20px',
+                    padding: '30px'
+                }} className="my-5">
+                    <TicketsForm />
+                </section>
+
+
+            </Container>
+        </>
     )
 }
 
-export default TicketsView
+export default hasPageHeaderFooter(TicketsView, {
+    viewTitle: 'Tickets',
+    pageHeaderImage: ticket,
+    pageFooterImage: ticket
+})
