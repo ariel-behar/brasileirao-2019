128a29149921e0cce81b5bdf818eec55851b2c0f
Ariel Behar <ariel.behar@hotmail.com>
2024-02-07 22:06:50 +0200

    Optimizing HomeView for mobile

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import soccerField from '../assets/img/backgrounds/soccer-field.jpg'
 import PlayerKicking from '../components/ViewSpecificComponents/HomeView/PlayerKicking'
 
@@ -11,36 +12,51 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row"
 import Container from "react-bootstrap/Container";
 import Footer from '../components/Footer/Footer'
+import styled from '@emotion/styled'
+
+const StyledContainer = styled(Container)`
+	background-image: url(${soccerField});
+	background-repeat: no-repeat;
+	background-size: cover;
+	height: 100%;
+	position: relative;
+
+	padding: 150px 10px 70px 10px;
+
+	@media (min-width: 768px) {
+		background-position: 'center';
+	}
+`
 
 const upcomingMatch: Event = events[0];
 
 function HomeView() {
 	return (
-		<Container fluid style={{
-			backgroundImage: `url('${soccerField}')`,
-			backgroundRepeat: 'no-repeat',
-			backgroundSize: 'cover',
-			height: '100%',
-			position: 'relative'
-		}}>
+		<StyledContainer fluid >
 			<HelmetSEO title="HOME" description="Home page of the fictional website about the Brasileirao 2019 Soccer Championship" />
 
-			<Row className='h-100 d-flex align-items-center'>
-				<Col lg={3} className='d-none d-lg-flex align-items-end'>
+			<Row className='h-100 d-flex align-items-center g-5'>
+				<Col lg={3} xl={4} className='d-none d-lg-flex align-items-end justify-content-center'>
 					<PlayerKicking />
 				</Col>
 
-				<Col xs={12} lg={{ span: 4, offset: 1 }}>
-					<FootballMatchCard event={upcomingMatch} displayOnView='home' />
+				<Col xs={12} md={6} lg={5} xl={{ span: 4 }} >
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1, delay: 2 }}
+					>
+						<FootballMatchCard event={upcomingMatch} displayOnView='home' />
+					</motion.div>
 				</Col>
 
-				<Col xs={12} lg={3} className='mx-auto'>
+				<Col xs={12} md={6} lg={3} className='mx-auto'>
 					<RankingDisplay />
 				</Col>
 			</Row>
 
 			<Footer />
-		</Container>
+		</StyledContainer>
 
 	)
 }
