132f2c36b95e3934556ba5b290f27506db57d846
Ariel Behar <ariel.behar@hotmail.com>
2024-02-08 11:12:46 +0200

    rearranging imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 import './styles/App.scss'
 
@@ -8,7 +9,6 @@ import router from './router/router'
 
 import Loader from './components/Loader'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
 import {faCalendar, faClock, faLocationDot} from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCalendar, faClock, faLocationDot)
