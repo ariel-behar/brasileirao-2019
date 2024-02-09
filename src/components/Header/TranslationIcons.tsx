5dbd4e354e026edff37a627666cfb35479742753
Ariel Behar <ariel.behar@hotmail.com>
2024-02-09 07:52:45 +0200

    Implementing the changeAppLanguage functionality to the TranslationIcons

--- src/components/Header/TranslationIcons.tsx
+++ src/components/Header/TranslationIcons.tsx
@@ -1,13 +1,32 @@
+import { motion }from 'framer-motion'
 import flagBrasil from '../../assets/img/flags/brazil.jpg'
 import flagBritain from '../../assets/img/flags/britain.jpg'
+import { useLanguageContext } from '../../contexts/languageContext'
 
 function TranslationIcons() {
-  return (
-    <>
-        <img style={{cursor: 'pointer'}} className='mx-2' src={flagBrasil} alt="Brasilian Flag button" />
-        <img style={{cursor: 'pointer'}} className='mx-2' src={flagBritain} alt="British Flag button" />
-    </>
-  )
+	const { changeAppLanguage } = useLanguageContext()
+
+	return (
+		<>
+			<motion.img
+				whileHover={{ scale: 1.1, boxShadow: '0px 0px 8px rgb(134, 134, 134)'}}
+				onClick={() => changeAppLanguage('pt')}
+				style={{ cursor: 'pointer' }}
+				className='mx-2'
+				src={flagBrasil}
+				alt="Brasilian Flag button"
+			/>
+
+			<motion.img
+				whileHover={{ scale: 1.1, boxShadow: '0px 0px 8px rgb(134, 134, 134)'}}
+				onClick={() => changeAppLanguage('en')}
+				style={{ cursor: 'pointer' }}
+				className='mx-2'
+				src={flagBritain}
+				alt="British Flag button"
+			/>
+		</>
+	)
 }
 
 export default TranslationIcons
