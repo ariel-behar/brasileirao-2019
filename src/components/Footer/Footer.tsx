23199084fa577524c7c37da4a1ecabb777499a72
Ariel Behar <ariel.behar@hotmail.com>
2024-02-10 21:55:33 +0200

    Fixing link in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled"
-import arielbeharLogo from "../../assets/img/logos/arielbehar-logo.png"
 import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom"
+
+import arielbeharLogo from "../../assets/img/logos/arielbehar-logo.png"
 
 const StyledFooter = styled('footer')`
     position: absolute;
@@ -25,7 +27,7 @@ function Footer() {
         <StyledFooter className="w-100 d-flex flex-row justify-content-center ">
             <p className="text-custom-light-gray fw-bold">
                 &copy; {t('copyright', { ns: "Footer", defaultValue: "2020 All Rights Reserved"})}&nbsp;
-                <a  href="www.arielbehar.com" target="_blank" rel="noreferrer">Ariel Behar</a>
+                <Link to="https://www.arielbehar.com/" target="_blank" rel="noreferrer">Ariel Behar</Link>
                 <img className="ms-1" style={{ height: '20px' }} src={arielbeharLogo} alt="Ariel Behar Logo" />
             </p>
         </StyledFooter>
