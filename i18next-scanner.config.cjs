c5a461ab0fbe060ef890057464120054dc9090e5
Ariel Behar <ariel.behar@hotmail.com>
2024-02-10 12:36:34 +0200

    updating config's settings

--- i18next-scanner.config.cjs
+++ i18next-scanner.config.cjs
@@ -1,29 +1,37 @@
 module.exports = {
-    input: [
-      "src/**/*.{js,jsx,ts,tsx}",
-      // Use ! to filter out files or directories
-      "!src/**/*.spec.{js,jsx,ts,tsx}",
-      "!i18n/**",
-      "!**/node_modules/**",
-    ],
-    output: "./",
-    options: {
-      compatibilityJSON: "v3",
-      debug: true,
-      func: {
-        list: ["i18next.t", "i18n.t", "t"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-      trans: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-      lngs: ["en", "pt"],
-      ns: ["translation"],
-      defaultLng: "en",
-      defaultNs: "translation",
-      resource: {
-        loadPath: "public/locales/{{lng}}/{{ns}}.json",
-        savePath: "public/locales/{{lng}}/{{ns}}.json",
-      },
-    },
-  };
+	input: [
+		"src/**/*.{js,jsx,ts,tsx}",
+		// Use ! to filter out files or directories
+		"!src/**/*.spec.{js,jsx,ts,tsx}",
+		"!i18n/**",
+		"!**/node_modules/**",
+	],
+	output: "./",
+	options: {
+		compatibilityJSON: "v3",
+		debug: true,
+		func: {
+			list: ["i18next.t", "i18n.t", "t"],
+			extensions: [".js", ".jsx", ".ts", ".tsx"],
+		},
+		trans: {
+			extensions: [".js", ".jsx", ".ts", ".tsx"],
+		},
+		lngs: ["en", "pt"],
+		ns: ["Header", "FootballMatchCard"],
+		defaultLng: "en",
+		defaultNs: "translation",
+		resource: {
+			loadPath: "public/locales/{{lng}}/{{ns}}.json",
+			savePath: "public/locales/{{lng}}/{{ns}}.json",
+			jsonIndent: 2,
+			lineEnding: '\n',
+		},
+		nsSeparator: ':',
+		keySeparator: '.',
+		interpolation: {
+			prefix: '{{',
+			suffix: '}}'
+		}
+	},
+};
