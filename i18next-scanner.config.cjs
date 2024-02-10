854da680c3d4a77216e55dcf45f8bf4a91d36119
Ariel Behar <ariel.behar@hotmail.com>
2024-02-10 10:48:39 +0200

    Creating the i18next-scanner config file

---
+++ i18next-scanner.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    input: [
+      "src/**/*.{js,jsx,ts,tsx}",
+      // Use ! to filter out files or directories
+      "!src/**/*.spec.{js,jsx,ts,tsx}",
+      "!i18n/**",
+      "!**/node_modules/**",
+    ],
+    output: "./",
+    options: {
+      compatibilityJSON: "v3",
+      debug: true,
+      func: {
+        list: ["i18next.t", "i18n.t", "t"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      trans: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      lngs: ["en", "pt"],
+      ns: ["translation"],
+      defaultLng: "en",
+      defaultNs: "translation",
+      resource: {
+        loadPath: "public/locales/{{lng}}/{{ns}}.json",
+        savePath: "public/locales/{{lng}}/{{ns}}.json",
+      },
+    },
+  };
