91393830444f81dd62214b181ab859a25c1df626
Ariel Behar <ariel.behar@hotmail.com>
2024-02-10 12:35:55 +0200

    Update i18n.ts

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -8,7 +8,7 @@ i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        
+        saveMissing: true,
         supportedLngs: ['en', 'pt'],
         debug: true,
         fallbackLng: 'en',
