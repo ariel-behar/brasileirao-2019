78d374254e84248715e53240b5750916f9549f92
Ariel Behar <ariel.behar@hotmail.com>
2024-02-11 13:39:46 +0200

    Fixing title's positions on different screen sizes

--- src/components/ViewTitle.tsx
+++ src/components/ViewTitle.tsx
@@ -2,17 +2,23 @@ import styled from "@emotion/styled"
 
 const StyledPageHeaderTitleDiv = styled('div')`
     position: absolute;
-    top: -12vh;
+    top: -10vh;
     left: 12vw;
     z-index: 10;
+
+    @media(min-width: 768px) {
+        top: -12vh;
+        left: 15vw;
+    }
+
 `
 interface Props {
-    viewTitle: "League" | "Teams" | "Fan Club" | "Events" | "Tickets" 
+    viewTitle: "League" | "Teams" | "Fan Club" | "Events" | "Tickets"
 }
 
 function ViewTitle({
     viewTitle,
-}:Props) {
+}: Props) {
     return (
         <StyledPageHeaderTitleDiv >
             <h2 className='page-header-title'>{viewTitle}</h2>
