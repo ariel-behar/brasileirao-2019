d47d899f0158d2a58f75e211ac8e464513ff990d
Ariel Behar <ariel.behar@hotmail.com>
2024-02-11 09:52:38 +0200

    Adding ScrollRestoration

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import TranslationIcons from '../components/Header/TranslationIcons'
 
 function RootView() {
 	return (
 		<>
+			<ScrollRestoration />
 			<span
 				style={{
 					position: 'absolute',
